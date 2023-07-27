27197466ebfc09b19f4d9cd13fac85f0ef5a9b6f
Saul Anthony Carbonell <saulcarbonell@gmail.com>
2023-07-27 13:51:47 +0100

    Updated code with move history

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ let gameBoard = document.getElementById('gameboard');
 let infoDisplay = document.getElementById('infoboard');
 let startingCells = ['', '', '', '', '', '', '', '', ''];
 let turn = 'O';
-let moveTracker = [];
 infoDisplay.textContent = 'O goes first';
 let gameEnds = false;
+let moveHistory = [];
+let currentMoveIndex = -1;
+
+let controlContainer = document.getElementById('controls');
+let prevButton = document.createElement('button');
+prevButton.innerHTML = '<i class="fa-solid fa-arrow-left"></i>';
+let nextButton = document.createElement('button');
+nextButton.innerHTML = '<i class="fa-solid fa-arrow-right"></i>';
+controlContainer.appendChild(prevButton);
+controlContainer.appendChild(nextButton);
+prevButton.setAttribute('class', 'control-buttons');
+nextButton.setAttribute('class', 'control-buttons');
+controlContainer.style.display = 'none';
+
+let resetButton = document.createElement('button');
+resetButton.textContent = 'RESET';
+resetButton.setAttribute('id', 'reset-button');
+resetButton.addEventListener('click', resetGame);
+document.body.appendChild(resetButton);
 
 function createBoard() {
   startingCells.forEach((cell, index) => {
@@ -22,29 +40,33 @@ function checkForDraw() {
 }
 
 function takeTurn(e) {
-    if (gameEnds || e.target.firstChild || getWinner()) return;
-  
-    const gameDisplay = document.createElement('div');
-    gameDisplay.classList.add(turn);
-    e.target.append(gameDisplay);
-  
-    if (checkForDraw() && !getWinner()) {
-      infoDisplay.textContent = "It's a draw!";
-      gameEnds = true;
-      removeClickListeners();
-      return;
-    }
-  
+  if (gameEnds || e.target.firstChild || getWinner()) return;
+
+  const gameDisplay = document.createElement('div');
+  gameDisplay.classList.add(turn);
+  e.target.append(gameDisplay);
+
+  if (checkForDraw() && !getWinner()) {
+    infoDisplay.textContent = "It's a draw!";
+    gameEnds = true;
+  } else {
     if (turn === 'O') {
       turn = 'X';
     } else {
       turn = 'O';
     }
     infoDisplay.textContent = turn + "'s turn now.";
-  
-    e.target.removeEventListener('click', takeTurn);
-    getWinner();
   }
+
+  e.target.removeEventListener('click', takeTurn);
+  getWinner();
+
+
+  const position = parseInt(e.target.id);
+  moveHistory.push(position);
+  currentMoveIndex = moveHistory.length - 1;
+  updateButtons();
+}
 
 function getWinner() {
   const allSquares = document.querySelectorAll('.square');
@@ -70,85 +92,76 @@ function getWinner() {
 
   if (winner) {
     infoDisplay.textContent = winner + ' wins!';
-    return winner;
-  }
-
-  return null;
-}
-
-function takeTurn(e) {
-  if (gameEnds || e.target.firstChild || getWinner()) return;
-
-  const gameDisplay = document.createElement('div');
-  gameDisplay.classList.add(turn);
-  e.target.append(gameDisplay);
-
-  // Update the move tracker with the current move
-  const move = turn + ' placed on cell ' + e.target.id;
-  moveTracker.push(move);
-
-  if (checkForDraw() && !getWinner()) {
-    infoDisplay.textContent = "It's a draw!";
     gameEnds = true;
+  }
+
+  if (gameEnds) {
+    controlContainer.style.display = 'flex';
     removeClickListeners();
-    return;
   }
 
-  if (turn === 'O') {
-    turn = 'X';
-  } else {
-    turn = 'O';
-  }
-  infoDisplay.textContent = turn + "'s turn now.";
-
-  e.target.removeEventListener('click', takeTurn);
-  getWinner();
-
-  const winner = getWinner();
-  if (winner) {
-    infoDisplay.textContent = winner + ' wins!';
-    gameEnds = true;
-    removeClickListeners();
-    moveTracker.push(winner + ' wins!');
-  } else {
-    infoDisplay.textContent = turn + "'s turn now.";
-    moveTracker.push(turn + ' placed on cell ' + e.target.id);
-  }
-
-  e.target.removeEventListener('click', takeTurn);
-  // Show the move tracker after the turn
-  showMoveTracker();
+  return gameEnds;
 }
-
-function showMoveTracker() {
-  const moveTrackerElement = document.getElementById('moveTracker');
-  moveTrackerElement.textContent = 'Move Tracker:\n';
-  moveTracker.forEach((move, index) => {
-    moveTrackerElement.textContent += (index + 1) + '. ' + move + '\n';
-  });
-} 
-
 
 function removeClickListeners() {
   const allSquares = document.querySelectorAll('.square');
   allSquares.forEach(square => square.removeEventListener('click', takeTurn));
 }
 
-const resetButton = document.createElement('button');
-resetButton.textContent = 'RESET';
-resetButton.setAttribute('id', 'reset-button')
-resetButton.addEventListener('click', resetGame);
-document.body.appendChild(resetButton);
+function clearBoard() {
+  const allSquares = document.querySelectorAll('.square');
+  allSquares.forEach(square => square.textContent = '');
+}
+
+function updateButtons() {
+  prevButton.disabled = currentMoveIndex <= -1;
+  nextButton.disabled = currentMoveIndex >= moveHistory.length - 1;
+}
+
+function applyMoveFromHistory() {
+  clearBoard();
+  const allSquares = document.querySelectorAll('.square');
+  moveHistory.slice(0, currentMoveIndex + 1 ).forEach((position, index) => {
+    const player = index % 2 === 0 ? 'O' : 'X';
+    const gameDisplay = document.createElement('div');
+    gameDisplay.classList.add(player);
+    allSquares[position].appendChild(gameDisplay);
+  });
+  turn = currentMoveIndex % 2 === 0 ? 'O' : 'X';
+  infoDisplay.textContent = `Move ${currentMoveIndex + 1}/${moveHistory.length} - ${turn}'s turn now.`;
+  getWinner();
+  updateButtons();
+}
+
+function showPreviousMove() {
+  if (currentMoveIndex >= 0) {
+    currentMoveIndex--;
+    applyMoveFromHistory();
+  }
+}
+
+function showNextMove() {
+  if (currentMoveIndex < moveHistory.length - 1) {
+    currentMoveIndex++;
+    applyMoveFromHistory();
+  }
+}
 
 function resetGame() {
+  controlContainer.style.display = 'none';
   const allSquares = document.querySelectorAll('.square');
   allSquares.forEach(square => square.textContent = '');
   turn = 'O';
   infoDisplay.textContent = 'O goes first';
   gameEnds = false;
   allSquares.forEach(square => square.addEventListener('click', takeTurn));
-   moveTracker = [];
-  showMoveTracker();
+  moveHistory = [];
+  currentMoveIndex = -1;
 }
 
+
+prevButton.onclick = showPreviousMove;
+nextButton.onclick = showNextMove;
+
 createBoard();
+
